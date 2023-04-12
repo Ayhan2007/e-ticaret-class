40b136416155597b728dc30a22ab83e53d5df359
ayhan <demirtas.ayhan2007@gmail.com>
2023-04-12 23:28:04 +0300

    ekleme

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,28 @@
 //// burada google firebase ile etkileşime buradan girilecek.
+//firebase ile etkileşime geçildi
+import { initializeApp } from "firebase/app";
+//authentication işlemleri için(kullanıcı kaydı)
+import { getAuth } from "firebase/auth";
+//firestore database erişimi için
+import {getFirestore} from "firebase/firestore";
+// storage erişimi için (resim kayıt yeri)
+import { getStorage } from "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACrGbya9BlhfnTAaEsAXWA0Mpb_Uo3Yqg",
+  authDomain: "e-ticaret-class-b3935.firebaseapp.com",
+  projectId: "e-ticaret-class-b3935",
+  storageBucket: "e-ticaret-class-b3935.appspot.com",
+  messagingSenderId: "963231621637",
+  appId: "1:963231621637:web:3031255d62f6613bc0326c",
+  measurementId: "G-JF7WQ41C44"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,9 +1,23 @@
 ////şifre resetleme sayfası
 import React from 'react'
+import styles from "./auth.module.scss"
+import resetImg from "../../assets/forgot.png"
+import Card from "../../components/card/Card"
 
 const Reset = () => {
   return (
-    <div>Reset</div>
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={resetImg} alt="Reset" width="400"/>
+      </div>
+      <Card cardClass={styles.form}>
+        <h2>Reset</h2>
+        <button className="--btn --btn-primary --btn-block">Reset Password</button>
+        <div className={styles.links}>
+
+        </div>
+      </Card>
+    </section>
   )
 }
 
