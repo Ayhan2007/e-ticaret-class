faf62b3d5253d70c759ad13d36500cc980d4523e
ayhan <demirtas.ayhan2007@gmail.com>
2023-04-18 14:01:00 +0300

    17.04.2023

--- src/components/hiddenLink/hiddenLink.js
+++ src/components/hiddenLink/hiddenLink.js
@@ -1,1 +1,21 @@
 ////burada kişi login yaptığında header kısmında ismini, myorders kısmını ve logout linkini gösterecek çıktığında tekrar eski haline getirecek login ve logout componentleri oluşturulacak
+import {useSelector} from "react-redux";
+import { slectIsLoggedIn } from "../../redux/slice/authSlice";
+
+export const ShowOnLogin = ({children}) => {
+    const isLoggedIn = useSelector(slectIsLoggedIn)
+
+    if(isLoggedIn) {
+        return children
+    }
+    return null
+}
+
+export const ShowOnLogout =({children}) => {
+    const isLoggedIn = useSelector(slectIsLoggedIn)
+
+    if(!isLoggedIn) {
+        return children
+    }
+    return null
+}
